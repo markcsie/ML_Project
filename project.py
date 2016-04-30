2019fcbb7014fea5e86246afe95de472da806434
markcsie <markcsie@gmail.com>
2016-04-29 20:25:52 -0700

    Some TODO comments

--- project.py
+++ project.py
@@ -2,16 +2,25 @@ import pandas as pd
 import xgboost as xgb
 import pickle
 import os.path
+from sklearn.metrics import log_loss, roc_auc_score
 
 if __name__ == "__main__":
+    # TODO: Add ZeroSum feature
+
+    # TODO: Remove constant features
+
+    # TODO: Remove identical features
+
+    # TODO: Min Max cut in test data ??
+
+    # TODO: Cross validation?????
+
     if not os.path.isfile("classifier.p"):
+        train = pd.read_csv('data/train.csv')
         train_x = train[train.columns[1:-1]]  # remove ID column and TARGET column, 371 - 1 - 1
         train_y = train[train.columns[-1]]
-        print("Start Training")
-        train = pd.read_csv('data/train.csv')
 
-        # TODO:Remove constant features
-
+        # TODO: Parameter explaination?????
         params = {}
         params["objective"] = "binary:logistic"
         params["booster"] = "gbtree"
@@ -22,8 +31,17 @@ if __name__ == "__main__":
         params["colsample_bytree"] = 0.701
 
         train_data = xgb.DMatrix(train_x, train_y)
-        watchlist = [(train_data, 'train')]
-        classifier = xgb.train(params = params, dtrain = train_data, num_boost_round = 560, evals = watchlist, verbose_eval=False)
+        print("Start Training")
+        classifier = xgb.train(params = params, dtrain = train_data, num_boost_round = 560, verbose_eval = False)
+
+        train_pred = classifier.predict(train_data)
+        train_log_loss = log_loss(train_y, train_pred)
+        train_roc = roc_auc_score(train_y, train_pred)
+
+        print(train_log_loss)
+        print(train_roc)
+
+        pickle.dump(classifier, open("classifier_%f_%f.p" % (train_log_loss, train_roc), "wb"))
         pickle.dump(classifier, open("classifier.p", "wb"))
     else:
         classifier = pickle.load(open("classifier.p", "rb" ))
